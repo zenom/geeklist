ee00c6744ece073fef86b74fe66d199c4fb88625
Andy Holman <andy@conspyre.com>
2012-04-17 18:59:57 -0400

    Start working on the oauth connections.

--- spec/geeklist/client_spec.rb
+++ spec/geeklist/client_spec.rb
@@ -4,11 +4,21 @@ require 'ap'
 describe Geeklist::Client do
 
   before do
-    #@client = Geeklist::Client.new('1234', '1234')
+    Geeklist.token = nil
+    Geeklist.secret = nil
   end
 
 
-  it "should be able to setup via a configure block" do
+  it 'should be able to set token and secret outside a block' do
+    Geeklist.token = '1234'
+    Geeklist.secret = '1234'
+
+    Geeklist.token.must_equal '1234'
+    Geeklist.secret.must_equal '1234'
+  end
+
+
+  it 'should be able to setup via a configure block' do
     Geeklist.configure do |config|
       config.token = '1234'
       config.secret = '1234'
@@ -16,6 +26,13 @@ describe Geeklist::Client do
 
     Geeklist.token.must_equal '1234'
     Geeklist.secret.must_equal '1234'
+  end
+
+
+  describe "#oauth" do
+    it 'should pass' do
+      true
+    end
   end
 
 
